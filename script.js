19935e3ded1bc3264ad8f5e315a999a28f73f394
TLuigi003 <tluigi003.l3@gmail.com>
2019-10-26 13:42:09 +0200

    Fixed time format

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function convertGSTTToSRT(string) {
 }
 
 function formatTime(time) {
-    return String(time.hours).padStart(2, '0') + String(time.minutes).padStart(2, '0') + ':' + String(time.seconds).padStart(2, '0') + ',000';
+    return String(time.hours).padStart(2, '0')+ ':' + String(time.minutes).padStart(2, '0') + ':' + String(time.seconds).padStart(2, '0') + ',000';
 }
 
 function convertSecondStringToRealtime(string) {
