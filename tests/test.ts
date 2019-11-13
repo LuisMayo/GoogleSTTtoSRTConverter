9fb7c62a943922a4b6321117dc732b1c83c499a6
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2019-11-13 19:03:38 +0000

    Added v1 test

--- tests/test.ts
+++ tests/test.ts
@@ -2,18 +2,26 @@ import { convertGSTTToSRT } from "../lib/convertGSTTToSRT";
 import { expect } from "chai";
 import * as fs from 'fs';
 
-describe('convertTest', function() {
-  it('convertv2Trimmed', function() {
-    let result = convertGSTTToSRT(getv2TrimmedMock());
-    expect(result).equal(getv2TrimmedResponse());
-  });
+describe('completeFunctionTests', function () {
+    it('convertv1Complete', function () {
+        let result = convertGSTTToSRT(getv2TrimmedMock());
+        expect(result).equal(getResponse());
+    });
+    it('convertv2Trimmed', function () {
+        let result = convertGSTTToSRT(getv2TrimmedMock());
+        expect(result).equal(getResponse());
+    });
 });
+
+function getv1Complete() {
+    return fs.readFileSync('./mocks/v2Complete.json', { encoding: 'UTF-8' });
+}
 
 
 function getv2TrimmedMock() {
-    return fs.readFileSync('./mocks/v2Trimed.json', {encoding: 'UTF-8'});
+    return fs.readFileSync('./mocks/v2Trimed.json', { encoding: 'UTF-8' });
 }
 
-function getv2TrimmedResponse() {
-    return fs.readFileSync('./mocks/v2Trimed.response.txt', {encoding: 'UTF-8'});
+function getResponse() {
+    return fs.readFileSync('./mocks/v2Trimed.response.txt', { encoding: 'UTF-8' });
 }
